c9e7fd3fed6f7e485e9057d54bca9720f84bdfe4
Muhammad Adnan R <madnanrizqullah@gmail.com>
2022-08-21 06:41:06 +0700

    feat: initial commit

---
+++ _next/static/snofQ39xwMJ6LIzIrxO1e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/4-33b442816336c7ac.js","static/chunks/pages/index-bd078f1dd267ca64.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
